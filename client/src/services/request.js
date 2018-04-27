ef1a16c472c663e3374215f890403f9e8a11f1fb
Michaela-vE <michaelavanesveld@gmail.com>
2018-04-27 16:19:00 +0100

    added request.js

--- client/src/services/request.js
+++ client/src/services/request.js
@@ -1,1 +1,31 @@
-// a
+const Request = function(url) {
+  this.url = url;
+}
+
+Request.prototype.get = function (onComplete) {
+  const request = new XMLHttpRequest();
+  request.open('GET', this.url);
+  request.addEventListener('load', function() {
+    if(this.status != 200) {
+      return;
+    }
+    const response = JSON.parse(this.responseText);
+    onComplete(response);
+  });
+  request.send();
+}
+
+Request.prototype.post = function (onComplete, payload) {
+  const request = new XMLHttpRequest();
+  request.open('POST', this.url);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.addEventListener('load', function() {
+    if (request.status !== 201) return;
+    const response = JSON.parse(request.responseText);
+    onComplete(response);
+  });
+  const jsonPayload = JSON.stringify(payload);
+  request.send(jsonPayload);
+};
+
+module.exports = Request;
