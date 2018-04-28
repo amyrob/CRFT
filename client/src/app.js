ad1f59daa6ed7a38f9daabd0a2b477debcb3f1fa
Michaela-vE <michaelavanesveld@gmail.com>
2018-04-28 15:00:00 +0100

    app.js set up page structure, need to do pub_view and pub_data

--- client/src/app.js
+++ client/src/app.js
@@ -1,19 +1,31 @@
-// const PubView = require('./views/pub_view.js');
+const PubView = require('./views/pub_view.js');
 const PubData = require('./models/pub_data.js');
 const Request = require('./services/request.js');
 const MapWrapper = require('./views/map_wrapper.js');
 
-// const pubView = new PubView();
+const pubView = new PubView();
 const request = new Request('http://localhost:3000/');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-const mapContainer = document.querySelector('#main-map');
-const defaultCenter = { lat: 55.946962, lng: -3.201958 };
+  const mapContainer = document.querySelector('#main-map');
+  const defaultCenter = { lat: 55.946962, lng: -3.201958 };
 
-const map = new MapWrapper(mapContainer, defaultCenter, 0);
+  const map = new MapWrapper(mapContainer, defaultCenter, 0);
 
-const pubSelect = document.querySelector('#pub-select');
-const pubContainer = document.querySelector('#pub-list-container');
+  const pubSelect = document.querySelector('#pub-select');
+  const pubContainer = document.querySelector('#pub-list-container');
+
+  const pubData = new PubData('http://localhost:3000/crft/admin');
+
+  pubSelect.addEventListener('change', (evt) => {
+    const selectedIndex = evt.target.value;
+    const selectedPub = pubData.data[selectedIndex];
+    pubContainer.renderDetail(selectedPub);
+  });
+
+  pubData.getData((data) => {
+    pubView.renderSelect(data);
+  });
 
 });
