50d7bc8790d5b4f84d6fa2425c95906f4df654b4
Michaela-vE <michaelavanesveld@gmail.com>
2018-04-28 15:11:31 +0100

    finished pub data

--- client/src/models/pub_data.js
+++ client/src/models/pub_data.js
@@ -1,2 +1,16 @@
+const Request = require('../services/request.js');
 
-// module.exports = PubData;
+const PubData = function(url) {
+  this.url = url;
+  this.data = [];
+};
+
+PubData.prototype.getData = function(onComplete) {
+  const request = new Request(this.url);
+  request.get((data) => {
+    this.data = data;
+    onComplete(data);
+  });
+};
+
+module.exports = PubData;
