7ee1037f4d3f1c002355d0e7db872bc3278c6adb
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-04-28 11:47:47 +0100

    CRUD added to server.js

--- server.js
+++ server.js
@@ -15,12 +15,39 @@ MongoClient.connect('mongodb://localhost:27017', function (err, client) {
   };
   const db = client.db('CRFT_db');
   console.log('Connected to *CRFT*');
-  const pubCollection = db.collection('pubs');
+  const pubsCollection = db.collection('pubs');
 
-  server.post('/crft', function(req, res) {
+//INDEX ROUTE
+  server.get('/crft/admin', function(req, res) {
+    pubsCollection.find().toArray(function(err, allPubs){
+      if (err){
+        console.error(err);
+        res.status(500);
+        res.send();
+      };
+      res.json(allPubs);
+    });
+  });
+
+//SHOW ROUTE
+  server.get('/crft/admin/:id', function(req, res){
+    const id = req.params.id;
+    const objectID = ObjectID(id);
+    pubsCollection.findOne({_id:objectID}, function(err, result){
+      if (err){
+        console.error(err);
+        res.status(500);
+        res.send();
+      };
+      res.send(result);
+    })
+  })
+
+//CREATE ROUTE
+  server.post('/crft/admin', function(req, res) {
     const newPub = req.body;
 
-    pubCollection.save(newPub, function (err, res) {
+    pubsCollection.save(newPub, function (err, result) {
       if(err) {
         console.error(err);
         res.status(500);
@@ -31,6 +58,53 @@ MongoClient.connect('mongodb://localhost:27017', function (err, client) {
       res.json(result.ops[0]);
     });
   });
+
+//UPDATE ROUTE
+  server.put('crft/admin/:id', function(req, res){
+    const updatedPub = req.body;
+
+    const id = req.params.id;
+    const objectID = ObjectID(id);
+    pubsCollection.update({_id:objectID}, updatedPub, function(err, result){
+      if(err) {
+        console.error(err);
+        res.status(500);
+        res.send();
+      };
+      res.status(204);
+      res.send();
+    });
+
+  });
+
+//DESTROY ALL ROUTE
+  server.delete('/crft/admin', function(req, res){
+    pubsCollection.deleteMany({}, function(err, result){
+      if(err) {
+        console.error(err);
+        res.status(500);
+        res.send();
+      };
+      res.send();
+    });
+  });
+
+  //DESTROY ONE
+  server.delete('/crft/admin/:id', function(req, res){
+    const id = req.params.id;
+    const objectID = ObjectID(id);
+    pubsCollection.deleteOne({_id:objectID}, function(err, result){
+      if(err) {
+        console.error(err);
+        res.status(500);
+        res.send();
+      };
+      res.status(204);
+      res.send();
+    })
+  })
+
+
 
   server.listen(3000, function(){
     console.log("Listening on port 3000");
