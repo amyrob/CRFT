99856d8ff7d92f4fe99485ff2cac1124e2f316a2
Kishan B <kishanb_uk@hotmail.com>
2018-04-30 14:07:43 +0100

    geolocation api script added to HTML file

--- client/src/app.js
+++ client/src/app.js
@@ -4,13 +4,14 @@ const Request = require('./services/request.js');
 const MapWrapper = require('./views/map_wrapper.js');
 
 const request = new Request('http://localhost:3000/');
+const geoRequest = new Request('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyANgDsBmHtPFYb8GbPZu1hJnGn8cwTlXBQ');
 
 document.addEventListener('DOMContentLoaded', () => {
 
   const mapContainer = document.querySelector('#main-map');
   const defaultCenter = { lat: 55.946962, lng: -3.201958 };
 
-  const map = new MapWrapper(mapContainer, defaultCenter, 0);
+  const map = new MapWrapper(mapContainer, defaultCenter, 15);
 
   const pubSelect = document.querySelector('#pub-select');
   const pubContainer = document.querySelector('#pub-list-container');
